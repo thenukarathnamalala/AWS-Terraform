6be1b55792bfa6c9158e12fb9b2284cc34a7f60e
thenukarathnamalala <rathnamalalathenuka@gmail.com>
2026-06-16 08:29:32 +0530

    Add S3 backend and S3 module

---
+++ backend-setup/main.tf
@@ -0,0 +1,53 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "thenuka-aws-terraform-state-bucket"
+
+  tags = {
+    Name        = "thenuka-aws-terraform-state-bucket"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "terraform-state-locks"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}

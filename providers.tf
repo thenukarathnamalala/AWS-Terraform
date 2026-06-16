873c8fd7c4a710457557575ce6cb482a51055a51
thenukarathnamalala <rathnamalalathenuka@gmail.com>
2026-06-16 08:22:06 +0530

    Configure remote Terraform state backend

--- providers.tf
+++ providers.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.10.0"
 
+  backend "s3" {
+    bucket         = "thenuka-aws-terraform-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
